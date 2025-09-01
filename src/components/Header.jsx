55ed0255b8ba7928babbe1f0051dca2215bee7a8
siam114 <smsiamislam598@gmail.com>
2025-09-02 02:34:12 +0600

    install react icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import logo from '../assets/Images/logo.png'
 
 
 const Header = () => {
   return (
     <div className=''>
-      header
+      <img src={logo} alt="logo" className='w-[80px] md:w-[150px] object-cover' />
     </div>
   )
 }
