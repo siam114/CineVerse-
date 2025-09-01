5fe8c885f0297a4c5f2c257261ede4ca355579f3
siam114 <smsiamislam598@gmail.com>
2025-09-02 02:25:18 +0600

    added extenal css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Header from './components/Header'
 
 function App() {
 
   return (
-    <div className='text-3xl font-bold'>
-siam
+    <div className=''>
+      <Header/>
     </div>
   )
 }
