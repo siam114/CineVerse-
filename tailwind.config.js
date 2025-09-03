72a6398516f5e8a06cb9540eb11f758f6ff00931
siam114 <smsiamislam598@gmail.com>
2025-09-04 01:44:59 +0600

    hero section design complete

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+import scrollbarHide from 'tailwind-scrollbar-hide'
+
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [scrollbarHide],
+}
