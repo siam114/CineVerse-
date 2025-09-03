0e0a2f11110c49a2391e190cce6bea9547f5b63a
siam114 <smsiamislam598@gmail.com>
2025-09-04 01:59:52 +0600

    design production house component

--- src/components/ProductionHouse.jsx
+++ src/components/ProductionHouse.jsx
@@ -41,10 +41,22 @@ const ProductionHouse = () => {
         },
 
     ]
-    
+
   return (
-    <div>
-      ProductionHouse
+     <div className='flex gap-2 md:gap-5 p-2 px-5 md:px-16 '>
+        {productionHouseList.map((item)=>(
+            <div className='border-[2px] border-gray-600
+            rounded-lg hover:scale-110 transition-all duration-300
+            ease-in-out cursor-pointer relative shadow-xl 
+            shadow-gray-800
+            '>
+                 <video src={item.video} autoPlay loop playsInline muted 
+            className='absolute z-0  top-0 rounded-md 
+            opacity-0 hover:opacity-50'/> 
+                <img src={item.image} className='w-full z-[1] opacity-100' /> 
+               
+            </div>
+        ))}
     </div>
   )
 }
