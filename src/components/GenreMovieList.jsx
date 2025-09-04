0a198884363238716b5454d649c8fa8cc6aa4f3d
siam114 <smsiamislam598@gmail.com>
2025-09-05 03:29:13 +0600

    movie list component crete and design

--- src/components/GenreMovieList.jsx
+++ src/components/GenreMovieList.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import GenresList from '../Constant/GenresList'
+import MovieList from './MovieList'
 
 const GenreMovieList = () => {
   return (
@@ -9,7 +10,7 @@ const GenreMovieList = () => {
             <div className='p-8 px-8 md:px-16'>
                 <h2 className='text-[20px] text-white 
                 font-bold'>{item.name}</h2> 
-                {/* <MovieList genreId={item.id} index_={index} />    */}
+                <MovieList genreId={item.id} index_={index} />   
             </div>
         ))}
     </div>
