f8f6f770ccb328c1db65df43550a0d6c453fa827
siam114 <smsiamislam598@gmail.com>
2025-09-05 03:24:26 +0600

    import genre movie list on app file

--- src/components/GenreMovieList.jsx
+++ src/components/GenreMovieList.jsx
@@ -9,7 +9,7 @@ const GenreMovieList = () => {
             <div className='p-8 px-8 md:px-16'>
                 <h2 className='text-[20px] text-white 
                 font-bold'>{item.name}</h2> 
-                <MovieList genreId={item.id} index_={index} />   
+                {/* <MovieList genreId={item.id} index_={index} />    */}
             </div>
         ))}
     </div>
