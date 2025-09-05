9e69414e6d74d4f773dbda610bc393df93600226
siam114 <smsiamislam598@gmail.com>
2025-09-06 00:55:46 +0600

    formating header item

--- src/components/HeaderItem.jsx
+++ src/components/HeaderItem.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React from "react";
 
-function HeaderItem({name,Icon}) {
+function HeaderItem({ name, Icon }) {
   return (
-    <div className='text-white flex items-center gap-3
+    <div
+      className="text-white flex items-center gap-3
     text-[15px] font-semibold cursor-pointer hover:underline
-    underline-offset-8 mb-2'>
-        <Icon/>
-        <h2 className=''>{name}</h2>
+    underline-offset-8 mb-2"
+    >
+      <Icon />
+      <h2 className="">{name}</h2>
     </div>
-  )
+  );
 }
 
-export default HeaderItem
+export default HeaderItem;
